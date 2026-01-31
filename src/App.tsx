326cb773459b0a1604f9f22f72c7a7eca90f7e0b
EgorDushenkov <egorich241208@gmail.com>
2026-01-31 20:34:09 +0600

    stage2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   const [orderVotes, setOrderVotes] = useState<number>(0);
   const [changeVotes, setChangeVotes] = useState<number>(0);
   const [protocol, setProtocol] = useState<string>("");
+  const [timeLeft, setTimeLeft] = useState<number>(30);
+
   const finishElection = () => {
     let winner = "Ничья";
     if (orderVotes > changeVotes) winner = "Партия Порядка";
@@ -15,7 +18,19 @@ function App() {
     setIsOpen(false);
     setOrderVotes(0);
     setChangeVotes(0);
+    setTimeLeft(30);
   };
+
+  useEffect(() => {
+    let timer: any;
+    if (isOpen && timeLeft > 0) {
+      timer = setInterval(() => { setTimeLeft((prev) => prev - 1); }, 1000)
+    }
+    if (timeLeft === 0 && isOpen) {
+      finishElection();
+    }
+    return () => clearInterval(timer);}, [isOpen, timeLeft]);
+
   return (
     <>
       <div>
@@ -41,7 +56,8 @@ function App() {
               Партии Перемен ({changeVotes})
             </button>
             <p>Общее кол-во голосов: {orderVotes + changeVotes}</p>
-
+            <strong>Часы комиссии: {timeLeft} сек.</strong>
+            <p></p>
             <button onClick={finishElection} style={{ color: 'red' }}>
               Завершить досрочно
             </button>
