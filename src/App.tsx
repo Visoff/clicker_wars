69e54866228d7b006d1773370275c446158b649d
EgorDushenkov <egorich241208@gmail.com>
2026-01-31 20:08:51 +0600

    stage1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [orderVotes, setOrderVotes] = useState<number>(0);
+  const [changeVotes, setChangeVotes] = useState<number>(0);
+  const [protocol, setProtocol] = useState<string>("");
+  const finishElection = () => {
+    let winner = "Ничья";
+    if (orderVotes > changeVotes) winner = "Партия Порядка";
+    if (changeVotes > orderVotes) winner = "Партия Перемен";
+    const resultString = `Итог: ${winner} (${orderVotes} vs ${changeVotes})`;
+    setProtocol(resultString);
+    setIsOpen(false);
+    setOrderVotes(0);
+    setChangeVotes(0);
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Избирательный участок</h1>
+        {!isOpen ? (
+          <div>
+            <button onClick={() => setIsOpen(true)}>
+              Открыть участок
+            </button>
+            {protocol && (
+              <div style={{ marginTop: '20px', border: '1px solid gray' }}>
+                <h3>Последний протокол:</h3>
+                <p>{protocol}</p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => setOrderVotes(orderVotes + 1)}>
+              Партии Порядка ({orderVotes})
+            </button>
+            <button onClick={() => setChangeVotes(changeVotes + 1)}>
+              Партии Перемен ({changeVotes})
+            </button>
+            <p>Общее кол-во голосов: {orderVotes + changeVotes}</p>
+
+            <button onClick={finishElection} style={{ color: 'red' }}>
+              Завершить досрочно
+            </button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
